01fbb5710752bec70330efa2324d04e0f78a0520
PekoStarrr <heydavi36@gmail.com>
2026-08-15 11:15:18 -0300

    update main.py
    from flask import Flask, render_template, request, redirect, url_for
    import random
    import os
    
    app = Flask(__name__)
    
    @app.route("/")
    def home():
        return render_template("login.html")
    
    @app.route("/login", methods=["POST"])
    def login():
        nome = request.form.get("name")
        if nome == "admin" and request.form.get("senha") == "1234":
            return redirect(url_for('chat', canal_id=100))
        if nome:
            return redirect(url_for('chat', canal_id=random.randint(10**9, 10**10)))
        return "Erro", 403
    
    @app.route("/channels/@me/<int:canal_id>")
    def chat(canal_id):
        return render_template("dashboard.html", id_do_canal=canal_id)
    
    if __name__ == "__main__":
        port = int(os.environ.get("PORT", 5000))
        app.run(host="0.0.0.0", port=port)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import random
+import os
 
 app = Flask(__name__)
 
@@ -20,4 +21,6 @@ def login():
 def chat(canal_id):
     return render_template("dashboard.html", id_do_canal=canal_id)
 
-app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
